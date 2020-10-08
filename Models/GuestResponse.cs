002bd4b75a9ec3c068d17cf160278067a898db8b
Joel Henrique Silva Santos <joelhenrique2000@gmail.com>
2020-10-08 17:49:51 -0300

    Criando o GuestResponse Model

---
+++ Models/GuestResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace estudo_asp_net_core.Models {
+    public class GuestResponse {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public bool? WillAttend { get; set; }
+    }
+}
