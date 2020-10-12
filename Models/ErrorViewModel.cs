85a07eb21b06e33b1ffa9e19d7ea5e604cbb1011
Joel Henrique Silva Santos <joelhenrique2000@gmail.com>
2020-10-12 19:04:07 -0300

    commit inicial

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace estudo_asp_net_core.Models {
-    public class ErrorViewModel {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
