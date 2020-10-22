9368e3a6389ae6a19acc1cfbd18b760934f4244f
Joel Henrique Silva Santos <joelhenrique2000@gmail.com>
2020-10-22 10:10:38 -0300

    Removido o metodo estático do Model produto e adicionado o DataSource

--- 2. Testing ASP.NET Core application/Testing/SimpleApp/Models/Product.cs
+++ 2. Testing ASP.NET Core application/Testing/SimpleApp/Models/Product.cs
@@ -9,20 +9,13 @@ namespace SimpleApp.Models
     {
         public string Name { get; set; }
         public decimal? Price { get; set; }
-        
-        public static Product[] GetProducts()
-        {
-            Product kayak = new Product {
-                Name = "Kayak",
-                Price = 275M
-            };
+    }
 
-            Product lifejacket = new Product {
-                Name = "Lifejacket",
-                Price = 48.95M
+    public class ProductDataSource : IDataSource {
+        public IEnumerable<Product> Products =>
+            new Product[] { 
+                new Product { Name = "Kayak", Price = 275M },
+                new Product { Name  = "Lifejacket", Price = 48.95M }
             };
-
-            return new Product[] { kayak, lifejacket };
-        }
     }
 }
