81c9ed12bd1bb38d37f0a6505235e83f1f186148
Joel Henrique Silva Santos <joelhenrique2000@gmail.com>
2020-10-22 07:55:19 -0300

    Organizando pastas

---
+++ 1. Hello World/estudo-asp-net-core/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using estudo_asp_net_core.Models;
+using estudo_asp_net_core.Repository;
+using System.Linq;
+
+namespace estudo_asp_net_core.Controllers {
+    public class HomeController : Controller {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger) {
+            _logger = logger;
+        }
+
+        public IActionResult Index() {
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm() {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse) {
+            GuestResponseRepository.AddResponse(guestResponse);
+            if (ModelState.IsValid) {
+                GuestResponseRepository.AddResponse(guestResponse);
+                return View("Thanks", guestResponse);
+            } else {
+                return View();
+            }
+        }
+        
+        public ViewResult ListResponses() {
+            return View(GuestResponseRepository.Responses.Where(r => r.WillAttend == true));
+        }
+
+        public IActionResult Privacy() {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error() {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
