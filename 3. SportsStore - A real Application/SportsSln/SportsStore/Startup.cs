3f0844664365a275ee83d035735f4ef6c65081df
Joel Henrique Silva Santos <joelhenrique2000@gmail.com>
2020-10-26 12:33:18 -0300

    Criado o Configuration

--- 3. SportsStore - A real Application/SportsSln/SportsStore/Startup.cs
+++ 3. SportsStore - A real Application/SportsSln/SportsStore/Startup.cs
@@ -7,11 +7,20 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using SportsStore.Models;
 
 namespace SportsStore
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration) {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
