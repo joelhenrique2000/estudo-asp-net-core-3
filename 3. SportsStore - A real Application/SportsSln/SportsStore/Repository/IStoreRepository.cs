41e8aa6aa626f5fa09b9029911d27ca0fda1324f
Joel Henrique Silva Santos <joelhenrique2000@gmail.com>
2020-10-26 13:02:32 -0300

    Criado o repository

--- 3. SportsStore - A real Application/SportsSln/SportsStore/Repository/IStoreRepository.cs
+++ 3. SportsStore - A real Application/SportsSln/SportsStore/Repository/IStoreRepository.cs
@@ -1,9 +1,13 @@
-﻿using System;
+﻿using SportsStore.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace SportsStore.Repository {
-    public class IStoreRepository {
+namespace SportsStore.Repository
+{
+    public interface IStoreRepository
+    {
+        IQueryable<Product> Products { get; }
     }
 }
