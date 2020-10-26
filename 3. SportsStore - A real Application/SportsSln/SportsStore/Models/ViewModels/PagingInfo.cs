6a6ee23ce2947e4a3b1fbdee02452c48c2ae3c89
Joel Henrique Silva Santos <joelhenrique2000@gmail.com>
2020-10-26 16:04:09 -0300

    Criado o PageLinkTag helper

--- 3. SportsStore - A real Application/SportsSln/SportsStore/Models/ViewModels/PagingInfo.cs
+++ 3. SportsStore - A real Application/SportsSln/SportsStore/Models/ViewModels/PagingInfo.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace SportsStore.Models
+namespace SportsStore.Models.ViewModels
 {
     public class PagingInfo
     {
