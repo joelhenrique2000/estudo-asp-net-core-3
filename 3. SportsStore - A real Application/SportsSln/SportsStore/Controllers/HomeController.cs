9abdd1b7bc4b6fa24178e2150da942e791935720
Joel Henrique Silva Santos <joelhenrique2000@gmail.com>
2020-10-27 07:34:49 -0300

    Atualizado o método Index do Controller Home

--- 3. SportsStore - A real Application/SportsSln/SportsStore/Controllers/HomeController.cs
+++ 3. SportsStore - A real Application/SportsSln/SportsStore/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models.ViewModels;
 using SportsStore.Repository;
 using System;
 using System.Collections.Generic;
@@ -14,9 +15,17 @@ namespace SportsStore.Controllers {
             repository = repo;
         }
 
-        public IActionResult Index(int productPage = 1) => View(repository.Products
-            .OrderBy(p => p.ProductID)
-            .Skip((productPage - 1) * PageSize)
-            .Take(PageSize));
+        public IActionResult Index(int productPage = 1)
+            => View(new ProductsListViewModel {
+                Products = repository.Products
+                    .OrderBy(p => p.ProductID)
+                    .Skip((productPage - 1) * PageSize)
+                    .Take(PageSize),
+                PagingInfo = new PagingInfo {
+                    CurrentPage = productPage,
+                    ItemsPerPage = PageSize,
+                    TotalItems = repository.Products.Count()
+                }
+            });
     }
 }
