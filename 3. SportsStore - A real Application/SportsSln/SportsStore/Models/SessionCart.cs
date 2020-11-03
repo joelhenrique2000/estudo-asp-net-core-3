14229b87fb8c6d5e3173abfcc466c1749bf54b15
Joel Henrique Silva Santos <joelhenrique2000@gmail.com>
2020-11-02 22:47:00 -0300

    Criado o SessionCart

---
+++ 3. SportsStore - A real Application/SportsSln/SportsStore/Models/SessionCart.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using SportsStore.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore.Models
+{
+    public class SessionCart : Cart
+    {
+        public static Cart GetCart(IServiceProvider services)
+        {
+            ISession session = services.GetRequiredService<IHttpContextAccessor>()?
+ .HttpContext.Session;
+            SessionCart cart = session?.GestJson<SessionCart>("Cart")
+            ?? new SessionCart();
+            cart.Session = session;
+            return cart;
+        }
+    }
+}
